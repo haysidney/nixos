07aebb956d7e4c54ca5d692c093a8538c81ffcc3
Sidney Hays <tech@sidneyhays.com>
2023-10-31 15:58:42 -0400

    Got the touchpad toggle key working on hyprland.

--- hyprland.nix
+++ hyprland.nix
@@ -66,6 +66,42 @@
           ${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1
         '';
       };
+      "hypr/touchpad-toggle.sh" = {
+        mode = "0755";
+        text = ''
+          #!/bin/sh
+
+          HYPRLAND_DEVICE="asue120a:00-04f3:319b-touchpad"
+
+          if [ -z "$XDG_RUNTIME_DIR" ]; then
+            export XDG_RUNTIME_DIR=/run/user/$(id -u)
+          fi
+
+          export STATUS_FILE="$XDG_RUNTIME_DIR/touchpad.status"
+
+          enable_touchpad() {
+            printf "true" > "$STATUS_FILE"
+            notify-send -u normal "Enabling Touchpad"
+            hyprctl keyword "device:$HYPRLAND_DEVICE:enabled" true
+          }
+
+          disable_touchpad() {
+            printf "false" > "$STATUS_FILE"
+            notify-send -u normal "Disabling Touchpad"
+            hyprctl keyword "device:$HYPRLAND_DEVICE:enabled" false
+          }
+
+          if ! [ -f "$STATUS_FILE" ]; then
+            enable_touchpad
+          else
+            if [ $(cat "$STATUS_FILE") = "true" ]; then
+              disable_touchpad
+            elif [ $(cat "$STATUS_FILE") = "false" ]; then
+              enable_touchpad
+            fi
+          fi
+        '';
+      };
     };
     sessionVariables = {
       XCURSOR_SIZE = "48";
