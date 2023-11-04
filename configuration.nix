357b08cad5c25c9c1d962fcf818c6f415450cf9a
Sidney Hays <tech@sidneyhays.com>
2023-11-04 12:22:00 -0400

    Added xone.

--- configuration.nix
+++ configuration.nix
@@ -398,6 +398,8 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  hardware.xone.enable = true;
+
   networking = {
     hostName = "zephyrus";
     networkmanager.enable = true;
