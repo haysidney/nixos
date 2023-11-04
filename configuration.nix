1da344ac47140bef7d4a36d4517e5b88ed40f4d5
Sidney Hays <tech@sidneyhays.com>
2023-11-04 08:47:59 -0400

    Fixed issues with Steam not wanting to launch Gamescope. Tips from: https://github.com/NixOS/nixpkgs/issues/162562#issuecomment-1523177264

--- configuration.nix
+++ configuration.nix
@@ -549,7 +549,32 @@
   };
 
   programs = {
-    steam.enable = true;
+    steam = {
+      enable = true;
+      package = pkgs.steam.override {
+        extraEnv = {};
+        extraLibraries = pkgs: with pkgs; [
+          xorg.libXcursor
+          xorg.libXi
+          xorg.libXinerama
+          xorg.libXScrnSaver
+          libpng
+          libpulseaudio
+          libvorbis
+          stdenv.cc.cc.lib
+          libkrb5
+          keyutils
+        ];
+      };
+      gamescopeSession = {
+        enable = true;
+        args = [
+          "--rt"
+          "-f"
+          "-o 10"
+        ];
+      };
+    };
     git.enable = true;
     bash = {
       promptInit = ''
