6d8f397d10572098b930638a983cf2e968ae7467
Sidney Hays <tech@sidneyhays.com>
2023-11-07 09:44:59 -0500

    Added gnupg and added some yubikey udev stuff.

--- configuration.nix
+++ configuration.nix
@@ -29,6 +29,7 @@
     "L+ /home/sidney/.config/transmission         - sidney users - /persist/home/.config/transmission"
     "L+ /home/sidney/.config/xsettingsd           - sidney users - /persist/home/.config/xsettingsd"
     "L+ /home/sidney/.factorio                    - sidney users - /persist/home/.factorio"
+    "L+ /home/sidney/.gnupg                       - sidney users - /persist/home/.gnupg"
     "L+ /home/sidney/.gtkrc-2.0                   - sidney users - /persist/home/.gtkrc-2.0"
     "L+ /home/sidney/.icons                       - sidney users - /persist/home/.icons"
     "d  /home/sidney/.local                    0755 sidney users"
@@ -471,56 +472,60 @@
         }
       ];
     };
-    udev.extraRules = ''
-      # HW.1 / Nano
-      SUBSYSTEMS=="usb", ATTRS{idVendor}=="2581", ATTRS{idProduct}=="1b7c|2b7c|3b7c|4b7c", TAG+="uaccess", TAG+="udev-acl"
-      # Blue
-      SUBSYSTEMS=="usb", ATTRS{idVendor}=="2c97", ATTRS{idProduct}=="0000|0000|0001|0002|0003|0004|0005|0006|0007|0008|0009|000a|000b|000c|000d|000e|000f|0010|0011|0012|0013|0014|0015|0016|0017|0018|0019|001a|001b|001c|001d|001e|001f", TAG+="uaccess", TAG+="udev-acl"
-      # Nano S
-      SUBSYSTEMS=="usb", ATTRS{idVendor}=="2c97", ATTRS{idProduct}=="0001|1000|1001|1002|1003|1004|1005|1006|1007|1008|1009|100a|100b|100c|100d|100e|100f|1010|1011|1012|1013|1014|1015|1016|1017|1018|1019|101a|101b|101c|101d|101e|101f", TAG+="uaccess", TAG+="udev-acl"
-      # Aramis
-      SUBSYSTEMS=="usb", ATTRS{idVendor}=="2c97", ATTRS{idProduct}=="0002|2000|2001|2002|2003|2004|2005|2006|2007|2008|2009|200a|200b|200c|200d|200e|200f|2010|2011|2012|2013|2014|2015|2016|2017|2018|2019|201a|201b|201c|201d|201e|201f", TAG+="uaccess", TAG+="udev-acl"
-      # HW2
-      SUBSYSTEMS=="usb", ATTRS{idVendor}=="2c97", ATTRS{idProduct}=="0003|3000|3001|3002|3003|3004|3005|3006|3007|3008|3009|300a|300b|300c|300d|300e|300f|3010|3011|3012|3013|3014|3015|3016|3017|3018|3019|301a|301b|301c|301d|301e|301f", TAG+="uaccess", TAG+="udev-acl"
-      # Nano X
-      SUBSYSTEMS=="usb", ATTRS{idVendor}=="2c97", ATTRS{idProduct}=="0004|4000|4001|4002|4003|4004|4005|4006|4007|4008|4009|400a|400b|400c|400d|400e|400f|4010|4011|4012|4013|4014|4015|4016|4017|4018|4019|401a|401b|401c|401d|401e|401f", TAG+="uaccess", TAG+="udev-acl"
-      # Nano SP
-      SUBSYSTEMS=="usb", ATTRS{idVendor}=="2c97", ATTRS{idProduct}=="0005|5000|5001|5002|5003|5004|5005|5006|5007|5008|5009|500a|500b|500c|500d|500e|500f|5010|5011|5012|5013|5014|5015|5016|5017|5018|5019|501a|501b|501c|501d|501e|501f", TAG+="uaccess", TAG+="udev-acl"
-      # Ledger Stax
-      SUBSYSTEMS=="usb", ATTRS{idVendor}=="2c97", ATTRS{idProduct}=="6011", TAG+="uaccess", TAG+="udev-acl"
+    udev = {
+      packages = [ pkgs.yubikey-personalization ];
+      extraRules = ''
+        # HW.1 / Nano
+        SUBSYSTEMS=="usb", ATTRS{idVendor}=="2581", ATTRS{idProduct}=="1b7c|2b7c|3b7c|4b7c", TAG+="uaccess", TAG+="udev-acl"
+        # Blue
+        SUBSYSTEMS=="usb", ATTRS{idVendor}=="2c97", ATTRS{idProduct}=="0000|0000|0001|0002|0003|0004|0005|0006|0007|0008|0009|000a|000b|000c|000d|000e|000f|0010|0011|0012|0013|0014|0015|0016|0017|0018|0019|001a|001b|001c|001d|001e|001f", TAG+="uaccess", TAG+="udev-acl"
+        # Nano S
+        SUBSYSTEMS=="usb", ATTRS{idVendor}=="2c97", ATTRS{idProduct}=="0001|1000|1001|1002|1003|1004|1005|1006|1007|1008|1009|100a|100b|100c|100d|100e|100f|1010|1011|1012|1013|1014|1015|1016|1017|1018|1019|101a|101b|101c|101d|101e|101f", TAG+="uaccess", TAG+="udev-acl"
+        # Aramis
+        SUBSYSTEMS=="usb", ATTRS{idVendor}=="2c97", ATTRS{idProduct}=="0002|2000|2001|2002|2003|2004|2005|2006|2007|2008|2009|200a|200b|200c|200d|200e|200f|2010|2011|2012|2013|2014|2015|2016|2017|2018|2019|201a|201b|201c|201d|201e|201f", TAG+="uaccess", TAG+="udev-acl"
+        # HW2
+        SUBSYSTEMS=="usb", ATTRS{idVendor}=="2c97", ATTRS{idProduct}=="0003|3000|3001|3002|3003|3004|3005|3006|3007|3008|3009|300a|300b|300c|300d|300e|300f|3010|3011|3012|3013|3014|3015|3016|3017|3018|3019|301a|301b|301c|301d|301e|301f", TAG+="uaccess", TAG+="udev-acl"
+        # Nano X
+        SUBSYSTEMS=="usb", ATTRS{idVendor}=="2c97", ATTRS{idProduct}=="0004|4000|4001|4002|4003|4004|4005|4006|4007|4008|4009|400a|400b|400c|400d|400e|400f|4010|4011|4012|4013|4014|4015|4016|4017|4018|4019|401a|401b|401c|401d|401e|401f", TAG+="uaccess", TAG+="udev-acl"
+        # Nano SP
+        SUBSYSTEMS=="usb", ATTRS{idVendor}=="2c97", ATTRS{idProduct}=="0005|5000|5001|5002|5003|5004|5005|5006|5007|5008|5009|500a|500b|500c|500d|500e|500f|5010|5011|5012|5013|5014|5015|5016|5017|5018|5019|501a|501b|501c|501d|501e|501f", TAG+="uaccess", TAG+="udev-acl"
+        # Ledger Stax
+        SUBSYSTEMS=="usb", ATTRS{idVendor}=="2c97", ATTRS{idProduct}=="6011", TAG+="uaccess", TAG+="udev-acl"
 
-      # Allows non-root users to have raw access to Logitech devices.
-      # Allowing users to write to the device is potentially dangerous
-      # because they could perform firmware updates.
+        # Allows non-root users to have raw access to Logitech devices.
+        # Allowing users to write to the device is potentially dangerous
+        # because they could perform firmware updates.
 
-      ACTION != "add", GOTO="solaar_end"
-      SUBSYSTEM != "hidraw", GOTO="solaar_end"
+        ACTION != "add", GOTO="solaar_end"
+        SUBSYSTEM != "hidraw", GOTO="solaar_end"
 
-      # USB-connected Logitech receivers and devices
-      ATTRS{idVendor}=="046d", GOTO="solaar_apply"
+        # USB-connected Logitech receivers and devices
+        ATTRS{idVendor}=="046d", GOTO="solaar_apply"
 
-      # Lenovo nano receiver
-      ATTRS{idVendor}=="17ef", ATTRS{idProduct}=="6042", GOTO="solaar_apply"
+        # Lenovo nano receiver
+        ATTRS{idVendor}=="17ef", ATTRS{idProduct}=="6042", GOTO="solaar_apply"
 
-      # Bluetooth-connected Logitech devices
-      KERNELS == "0005:046D:*", GOTO="solaar_apply"
+        # Bluetooth-connected Logitech devices
+        KERNELS == "0005:046D:*", GOTO="solaar_apply"
 
-      GOTO="solaar_end"
+        GOTO="solaar_end"
 
-      LABEL="solaar_apply"
+        LABEL="solaar_apply"
 
-      # Allow any seated user to access the receiver.
-      # uaccess: modern ACL-enabled udev
-      TAG+="uaccess"
+        # Allow any seated user to access the receiver.
+        # uaccess: modern ACL-enabled udev
+        TAG+="uaccess"
 
-      # Grant members of the "plugdev" group access to receiver (useful for SSH users)
-      #MODE="0660", GROUP="plugdev"
+        # Grant members of the "plugdev" group access to receiver (useful for SSH users)
+        #MODE="0660", GROUP="plugdev"
 
-      LABEL="solaar_end"
-      # vim: ft=udevrules
-    '';
+        LABEL="solaar_end"
+        # vim: ft=udevrules
+      '';
+    };
     switcherooControl.enable = true;
     udisks2.enable = true;
+    pcscd.enable = true;
   };
   systemd.services = {
     keyd = {
@@ -554,6 +559,11 @@
   };
 
   programs = {
+    gnupg.agent = {
+      enable = true;
+      pinentryFlavor = "curses";
+      enableSSHSupport = true;
+    };
     steam = {
       enable = true;
       package = pkgs.steam.override {
